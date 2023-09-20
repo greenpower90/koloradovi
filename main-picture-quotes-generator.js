83161e49b72477a5c7a02dc8285456f7d335686e
greenpower90 <144276799+greenpower90@users.noreply.github.com>
2023-09-20 10:37:53 +0200

    Add files via upload

--- main-picture-quotes-generator.js
+++ main-picture-quotes-generator.js
@@ -7,7 +7,11 @@ window.onload = function (){
   'Images/5.jpg', 
   'Images/6.jpg', 
   'Images/7.png',
-  'Images/8.jpg'
+  'Images/8.jpg',
+  'Images/9.jpg',
+  'Images/10.jpg',
+  'Images/11.jpg',
+  'Images/12.jpg'
 ];
 const quotes = [{
   quote: `"Nic jiného, než energie čisté lásky a radosti nemůže dítěti pomoci v překonáni jeho karmy. Naše bezpodmínečná láska mu umožní pocit , že je plnohodnotným člověkem.."`,
@@ -22,7 +26,35 @@ const quotes = [{
 
   quote: `"Když je v sázce lidská bytost, tak člověk musí být trpělivý a ochotný se ještě učit, protože stát se může cokoliv. Navíc, když je ve hře ještě láska."`,
   writer: `(Tracyho tygr - William Saroyan)`
-}, 
+}, {
+
+  quote: `"Když je v sázce lidská bytost, tak člověk musí být trpělivý a ochotný se ještě učit, protože stát se může cokoliv. Navíc, když je ve hře ještě láska."`,
+  writer: `(Tracyho tygr - William Saroyan)`
+},{
+
+  quote: `"Je lepší dělat štěstí, než neštěstí."`,
+  writer: ``
+},{
+
+  quote: `"Trápení něhou povolí "`,
+  writer: ``
+},{
+
+  quote: `"Na otázku utrpení neexistuje odpověď intelektuální, ale pouze existenciální  - tedy odpověď samotným žitím"`,
+  writer: `(Petr Tavel)`
+},{
+
+  quote: `"Naděje je nakažlivá"`,
+  writer: `(Jane Goodall, Kniha o naději)`
+},{
+
+  quote: `"Být pochopen je jedna z nejvyšších forem intimity."`,
+  writer: ``
+},{
+
+  quote: `"Mylně věříme, že odvaha vyžaduje oběti. Ve skutečnosti, však největší oběť vyžaduje zbabělost."`,
+  writer: `(Odvážné snění - Albert Villoldo)`
+},
  ]
 
  let btn = document.querySelector("#Qbtn");
