b361499bcdf50552bcf9c9f077bb5811908297f4
greenpower90 <144276799+greenpower90@users.noreply.github.com>
2023-09-25 11:07:03 +0200

    Add files via upload

---
+++ patka.js
@@ -0,0 +1,8 @@
+fetch('../patka.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_patka");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
