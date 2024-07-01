d1a899c5fa0c579283d82b4e8119fe089ca1d423
greenpower90 <144276799+greenpower90@users.noreply.github.com>
2024-07-01 11:41:13 +0200

    adding simon game

---
+++ games/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,108 @@
+let buttonColours = ["red", "blue", "green", "yellow"]
+let gamePattern = []
+let userClickedPattern = []
+let gameIsOn = false
+let level = 0
+let userChosenColour
+let currentPosition = 0
+
+//Náhodný index barvy od 0 do 3
+function randomColor(){
+    let randomNumber = Math.floor(Math.random()*4)
+    return randomNumber
+}
+
+//přehraje zvuk na základě textu barvy
+function playSound(color){
+    let path = `/sounds/${color}.mp3`
+    let soundToPlay = new Audio(path)
+    soundToPlay.play()
+}
+
+function nextSequence(){
+        level++
+        updateTitle(level)
+        let randomChosenColour = buttonColours[randomColor()]
+        gamePattern.push(randomChosenColour)
+        console.log(`gamePattern ${gamePattern}`)
+        $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100)
+        playSound(randomChosenColour)
+}
+
+function animatePress(currentColour){
+    $(`#${currentColour}`).addClass("pressed")
+    
+    setTimeout(()=>{
+        $(`#${currentColour}`).removeClass("pressed")}
+        , 100)
+}
+
+function playerMove(e){
+    userChosenColour = e.target.id
+    console.log(`userChosenColor ${userChosenColour}`)
+    animatePress(userChosenColour)
+    userClickedPattern.push(userChosenColour)
+    console.log(userClickedPattern)
+    playSound(userChosenColour)
+}
+
+function updateTitle(level){
+$("#level-title").text(`Level ${level}`)
+}
+
+
+function checkAnswer(currentPosition){
+if(gamePattern[currentPosition]===userClickedPattern[currentPosition]){
+    console.log("correct")
+    return true
+}else{
+    console.log("wrong")
+    return false
+}
+}
+
+
+//Game Logic Code
+console.log(`GameIsOn  ${gameIsOn}`)
+
+$(document).keydown(()=>{
+    if (!gameIsOn){
+        gameIsOn = true
+        updateTitle(level)
+        nextSequence()
+    }
+
+})
+
+
+$(".btn").click((e)=>{
+    if (gameIsOn){
+        console.log(`gamePattern.length ${gamePattern.length}`)
+    
+        playerMove(e)
+        if(checkAnswer(currentPosition)){
+            currentPosition++
+            console.log(`currentPosition ${currentPosition}`)
+            
+            if(currentPosition === gamePattern.length){
+                currentPosition = 0
+                userClickedPattern = []
+                setTimeout(()=>{nextSequence()}, 1000)
+                
+            }
+        }else{
+            playSound('wrong')
+            $("#level-title").text(`GAME OVER, level ${level} - Press A Key to Start`)
+            resetGame()
+        }
+    }
+
+})
+
+
+function resetGame(){
+    gameIsOn = false
+    gamePattern = []
+    userClickedPattern = []
+    level = 0
+}
